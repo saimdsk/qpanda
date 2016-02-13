26211fbff390dce4dfa5e7297eb9308926c70a64
Yaseen <yaseenkadir@ymail.com>
2016-02-13 15:47:01 +1100

    Styling changes, namely answer button is right aligned under the text area.

--- qpanda/forms.py
+++ qpanda/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm, TextInput, Textarea
 
 from .models import Question, Answer
 
@@ -8,7 +8,11 @@ class QuestionForm(ModelForm):
         fields = ['question_text']
         # We only need to display the question text part of the form. The rest of the fields we assign server side.
         widgets = {
-            'question_text': TextInput(attrs={'class':'askbar', 'placeholder':'Ask A Question'}),
+            'question_text': TextInput(attrs={'class': 'askbar',
+                                              'placeholder': 'Ask A Question',
+                                              'autofocus': 'autofocus'}),
+            # autofocus is a boolean attr in html. But we have to submit it as a dict entry in django.
+
             # Because of the way forms work I just pass the form into the template and it knows what to render. Amazing!
             # But if you wish to make changes to the html element attributes you have to do it here.
         }
@@ -18,4 +22,9 @@ class AnswerForm(ModelForm):
     class Meta:
         model = Answer
         fields = ['answer_text']
+        widgets = {
+            'answer_text': Textarea(attrs={'class': 'answerbar',
+                                           'placeholder': 'Enter your answer',
+                                           'autofocus': 'autofocus'})
+        }
 
