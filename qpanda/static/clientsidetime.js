f14f97968cf432aea62f33e810e9995a0ed67fb2
Yaseen <yaseenkadir@ymail.com>
2016-02-18 01:53:04 +1100

    Typo, minutes not seconds.

--- qpanda/static/clientsidetime.js
+++ qpanda/static/clientsidetime.js
@@ -5,7 +5,7 @@
 $(document).ready(function() {
 
     var offset = new Date().getTimezoneOffset();
-    // offset returns the difference between UTC timezone and client timezone in seconds.
+    // offset returns the difference between UTC timezone and client timezone in minutes.
     // e.g. Sydney is 11 hours ahead (during AEDT) which is an offset of -660. Because Sydney is ahead in time compared
     // to UTC the offset is negative. It can be represented in UTC as UTC+11 though, because it is 11 hours ahead.
     var utcoffset = offset / 60.0 * -1;
