2fff76ca591abbefd1b1e14aae9248b796c27433
Yaseen <yaseenkadir@ymail.com>
2016-02-18 03:19:21 +1100

    Hover on how long ago question was asked will also show exact time, like answers. Renamed class so that jquery will convert both question asked time and answer time.

--- qpanda/static/qpanda.js
+++ qpanda/static/qpanda.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     // Instead of even using seconds/milliseconds since epoch django can convert datetimefield to an RFC 2822 compliant
     // formatted date, which javascript can use to construct a date. Even easier than before!
 
-    $('span.timeasked').each(function(i, obj) {
+    $('span.time').each(function(i, obj) {
         var rfc2822datestring = $(this).attr('title');
         var d = new Date(rfc2822datestring);
 
